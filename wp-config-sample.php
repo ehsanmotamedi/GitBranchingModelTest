26acadfc83f58b69f284b5fb8306e1276d82d8df
Suvethan Vignarajah <suvethan.vignarajah@bmo.com>
2015-11-05 14:15:48 -0500

    adding a strong password to the config

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'database_name_here');
 define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'weakPassword');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
